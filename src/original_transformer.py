8fde90d7e571a54d06cb085101876ed63da465cd
Adrian Atienza <72130704+adrianatienza1996@users.noreply.github.com>
2022-08-23 11:58:02 +0200

    Decoder has been implemented

--- src/original_transformer.py
+++ src/original_transformer.py
@@ -1,3 +1,4 @@
+from wsgiref import handlers
 import torch
 import torch.nn as nn
 
@@ -130,3 +131,70 @@ class Encoder(nn.Module):
                 do_prob=self.do_prob))
 
         return net
+
+
+
+#########################################################################################
+##################################### DECODER ###########################################
+#########################################################################################
+
+class DecoderBlock(nn.Module):
+
+    def __init__(self, number_heads=8, model_dim=512, do_prob=0.1):
+        super(DecoderBlock, self).__init__()
+        self.masked_mh_atten = MultiHead_Attention(number_heads=number_heads, 
+                                                    model_dim=model_dim,
+                                                    do_prob=do_prob)
+        self.add_norm_mmh = Add_and_Norm(model_dim=model_dim)
+        self.mh_atten = MultiHead_Attention(number_heads=number_heads, 
+                                                  model_dim=model_dim,
+                                                  do_prob=do_prob)
+        self.add_norm_mh = Add_and_Norm(model_dim=model_dim)
+        self.ffn = FeedForwardNet(model_dim=model_dim, 
+                                  do_prob=do_prob)
+        self.add_norm_ffn = Add_and_Norm(model_dim=model_dim)
+
+    
+    def forward(self, x, end_rep, mask):
+        res = x
+        h = self.masked_mh_atten(x, x, x, mask)
+        h = self.add_norm_mmh(h, res)
+        
+        res = h
+        h = self.mh_atten(end_rep, end_rep, h)
+        h = self.add_norm_mmh(h, res)
+
+        res = h
+        h = self.ffn(h)
+        return self.add_norm_ffn(h, res)
+
+
+
+class Decoder(nn.Module):
+    def __init__(self, num_blocks, number_heads=8, model_dim=512, do_prob=0.1):
+        super(Decoder, self).__init__()
+        self.num_blocks = num_blocks
+        self.num_heads = number_heads
+        self.model_dim = model_dim
+        self.do_prob = do_prob
+
+        self.net = self.create_net()
+
+    def forward(self, x, enc_rep, mask):
+        h = x
+        for layer in self.net:
+            h = layer(h, enc_rep, mask)
+
+        return h
+
+    def create_net(self):
+        net = nn.ModuleList()
+
+        for _ in range(self.num_blocks):
+            net.append(DecoderBlock(
+                number_heads=self.num_heads,
+                model_dim=self.model_dim,
+                do_prob=self.do_prob))
+
+        return net
+
